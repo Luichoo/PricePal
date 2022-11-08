bcea6c39b188d80dcf8c1e6c59e89f7253837700
Luis Blanco <slam46507@live.com.mx>
2022-11-07 21:29:05 -0600

    some stuff

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const port = 3000;
+
+const placesRoutes = require('./routes/places-routes');
+const app = express();
+
+app.use(placesRoutes);
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
+})
