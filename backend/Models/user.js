58dd8f83420254a0823834296f27d977afcf57b3
Luis Blanco <slam46507@live.com.mx>
2022-11-15 16:40:48 -0600

    se puso para exportar el modulo

--- backend/Models/user.js
+++ backend/Models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User', {
-    email: { type : String, required: true, unique: true },
-    password: { type : String, required: true, minlength: 5 },
-    salt: { type : String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 5 },
+    salt: { type: String, required: true },
 });
+
+module.exports = User;
