c905968b19fca91bf8a5c2ed60c41adcec617ccc
Luis Blanco <slam46507@live.com.mx>
2022-11-15 11:30:01 -0600

    modelo del usuario sin carro de compras

---
+++ backend/Models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+    email: { type : String, required: true, unique: true },
+    password: { type : String, required: true, minlength: 5 },
+    salt: { type : String, required: true },
+});
