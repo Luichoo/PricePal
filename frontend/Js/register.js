4f3bce08daab7a944d5db8533d1784a4436811b8
Luis Blanco <slam46507@live.com.mx>
2022-11-16 04:23:42 -0600

    js para registro

---
+++ frontend/Js/register.js
@@ -0,0 +1,22 @@
+const addformListener = () => {
+    const userForm = document.getElementById('user-form')
+    console.log(userForm)
+    userForm.onsubmit = async(e) => {
+        e.preventDefault()
+        const formData = new FormData(userForm)
+        const data = Object.fromEntries(formData.entries())
+        await fetch('/register', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        userForm.reset()
+        
+    }
+}
+
+window.onload = () => {
+    addformListener()
+}
