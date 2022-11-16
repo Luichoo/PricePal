ddef040a7ad99b2ee379a4badada3f96b31935ea
Luis Blanco <slam46507@live.com.mx>
2022-11-15 19:28:27 -0600

    Delete user.js

--- backend/Models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const User = mongoose.model('User', {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 5 },
-    salt: { type: String, required: true },
-});
-
-module.exports = User;
