2271e723fb716d30860088ab452c4c1074c20ffd
Luis Blanco <slam46507@live.com.mx>
2022-11-16 07:34:24 -0600

    se quitaron comentarios

--- backend/Routes/places-routes.js
+++ backend/Routes/places-routes.js
@@ -4,13 +4,8 @@ const placesControllers = require('../Controllers/places-controller')
 // This is a router, which is a middleware function that handles routes
 
 const router = express.Router();
-//router.get('/', placesControllers.getMedia);
-
 router.get('/', placesControllers.getRoot);
 router.get('/login', placesControllers.getLogin);
 router.get('/register', placesControllers.getRegister);
-// router.get('/:pid', placesControllers.getplaceById);
-
-//router.get('/user/:uid', placesControllers.getplaceByUserId);
 router.get('*', placesControllers.getError404);
 module.exports = router;
